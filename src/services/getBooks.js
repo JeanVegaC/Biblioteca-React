2bd954a94a76b5b8114d978a06aa75355f8531e6
Jhean Vega <jhonivegacasimiro@gmail.com>
2022-11-18 16:28:09 -0500

    add crud read

--- src/services/getBooks.js
+++ src/services/getBooks.js
@@ -1,6 +1,6 @@
 export const getBooks = async () => {
-    // let res = await fetch('https://dbbiblioteca.vercel.app/api/books'),
-    let res = await fetch('http://localhost:4000/api/books'),
+    // let res = await fetch('http://localhost:4000/api/books'),
+    let res = await fetch('https://dbbiblioteca.vercel.app/api/books'),
         json = await res.json();
 
         
